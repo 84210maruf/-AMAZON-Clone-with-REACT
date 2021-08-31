067e05dc5d02a4bcb9af59d07764ac098c79e5d9
maruf <maruf84210cse@gmail.com>
2021-08-31 18:49:43 +0600

    Before Firebase init 2nd time

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // Elements & loaadSripe is for payment Proccess
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-
 import { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
@@ -12,6 +11,7 @@ import Home from "./Home";
 import Login from "./Login";
 import Payment from "./Payment";
 import { useStateValue } from "./StateProvider";
+
 
 const promise = loadStripe(
   //This API keys from stripe.com
--- src/Payment.js
+++ src/Payment.js
@@ -1,13 +1,12 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import React, { useEffect, useState } from "react";
-import CurrencyFormat from 'react-currency-format';
-import { Link, useHistory } from "react-router-dom";
 import axios from "./axios";
+import React, { useEffect, useState } from "react";
+import CurrencyFormat from "react-currency-format";
+import { Link, useHistory } from "react-router-dom";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
-import { getBasketTotal } from './reducer';
+import { getBasketTotal } from "./reducer";
 import { useStateValue } from "./StateProvider";
-
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -18,50 +17,52 @@ function Payment() {
   const elements = useElements();
 
   const [succeeded, setSucceeded] = useState(false);
-  const [processing, setProcessing] = useState('');
+  const [processing, setProcessing] = useState("");
   const [error, setError] = useState(null);
   const [disabled, setDisabled] = useState(true);
   const [clientSecret, setClientSecret] = useState(true);
-
 
   useEffect(() => {
     //generate a special stripe secrete whice allows us charge a customer
     const getClientSecret = async () => {
       const response = await axios({
-        method: 'post',
-        // Stripe expects the total in a currencies
-        url: `/payments/create?total=${getBasketTotal(basket) * 100}`
+        method: "post",
+        // Stripe expects the total in a currencies subunits
+        url: `/payments/create?total=${getBasketTotal(basket) * 100}`,
       });
       setClientSecret(response.data.clientSecret);
-    }
+    };
 
     getClientSecret();
-  }, [basket])
+  }, [basket]);
 
 
   const handleSubmit = async (event) => {
     //Do all fancy Stripe Stuff...
+
     event.preventDefault();
     setProcessing(true);
-    const payload = await stripe.confirmCardPayment(clientSecret, {
-      payment_method: {
-        card: elements.getElement(CardElement)
-      }
-    }).then(({ paymentIntent }) => {
-      // paymentIntent = payment confirmetion
-      setSucceeded(true);
-      setError(null);
-      setProcessing(false);
-      history.replace('/orders');
-    })
-  }
 
+    const payload = await stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: elements.getElement(CardElement),
+        },
+      })
+      .then(({ paymentIntent }) => {
+        // paymentIntent = payment confirmetion
 
-  const handleChange = event => {
+        setSucceeded(true);
+        setError(null);
+        setProcessing(false);
+        history.replace("/orders");
+      });
+  };
+
+  const handleChange = (event) => {
     setDisabled(event.empty);
-    setError(event.error ? event.error.message : '');
-  }
-
+    setError(event.error ? event.error.message : "");
+  };
 
   return (
     <div className="payment">
@@ -76,9 +77,11 @@ function Payment() {
           </div>
           <div className="payment__address">
             <p>
-              {!user ? <Link to='/login'>
-              Signin Your Amazon Account
-              </Link> : user.email}
+              {!user ? (
+                <Link to="/login">Signin Your Amazon Account</Link>
+              ) : (
+                user.email
+              )}
             </p>
             <p>Suza, ullapara</p>
             <p>Dhaka, BD</p>
@@ -103,39 +106,39 @@ function Payment() {
         </div>
 
         <div className="payment__section">
-
           <div className="payment__title">
             <h3>Payment Method</h3>
           </div>
 
           <div className="payment__details">
-
             <form onSubmit={handleSubmit}>
-                <CardElement onChange={handleChange}/>
-                <div className='payment__priceContainer'>
-                    <CurrencyFormat
-                        renderText={(value) => (
-                          <>
-                            <p>
-                              Order Total : <strong> {value}</strong>
-                            </p>
-                          </>
-                        )}
-                        decimalScale={2}
-                        value={getBasketTotal(basket)}
-                        displayType={"text"}
-                        thousandSeparator={true}
-                        prefix={"$ "}
-                        suffix={" USD"}
-                    />
-                    <button 
-                    disabled={processing || disabled || succeeded || error}>
-                      <span /*onClick={handleSubmit}*/>{processing ? <p>Processing..</p> : 'Buy Now'}</span>
-                    </button>
-                </div>
-                {error && <div>{error}</div>}
+
+              <CardElement onChange={handleChange} />
+
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => (
+                    <>
+                      <p>
+                        Order Total : <strong> {value}</strong>
+                      </p>
+                    </>
+                  )}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$ "}
+                  suffix={" USD"}
+                />
+                <button disabled={processing || disabled || succeeded || error}>
+                  <span /*onClick={handleSubmit}*/>
+                    {processing ? <p>Processing..</p> : "Buy Now"}
+                  </span>
+                </button>
+              </div>
+              {error && <div>{error}</div>}
             </form>
-        
           </div>
         </div>
       </div>
